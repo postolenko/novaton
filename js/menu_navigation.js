f483d62c77156654174982438928de702a4a975b
postolenko <denis.postolenko@yandex.ua>
2016-07-18 04:14:22 +0300

    NovaTon

---
+++ js/menu_navigation.js
@@ -0,0 +1,21 @@
+$(".link-h").click(function(e) {
+
+	var indexItem = $( ".link-h" ).index( this );
+
+	if( $(".nav-item:eq("+ indexItem +") ul").hasClass("inner-nav")) {
+
+		e.preventDefault();	
+
+		if ( $(".nav-item:eq("+ indexItem +") > .inner-nav").height() > 0 ) {
+
+			$(".nav-item:eq("+ indexItem +") > .inner-nav").css({"height" : 0 });
+
+		} else {
+
+			$(".nav-item:eq("+ indexItem +") > .inner-nav").css({"height" : "auto"});
+
+		}
+
+	}
+
+});
